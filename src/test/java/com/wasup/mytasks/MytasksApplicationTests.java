d92c8a9ce70b16b6ec6bbe6ab01fc1168cb73a61
Guillaume DHOTEL <guillaume.dhotel1@gmail.com>
2025-07-23 18:25:55 +0200

    Initial commit

---
+++ src/test/java/com/wasup/mytasks/MytasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wasup.mytasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
