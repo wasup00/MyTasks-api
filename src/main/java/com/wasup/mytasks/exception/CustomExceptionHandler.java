d60f501601554fa7bd18fc157904302583dd81bb
Guillaume DHOTEL <guillaume.dhotel1@gmail.com>
2025-08-04 18:56:02 +0200

    add ErrorResponse model and refactor exception handling to return structured responses

--- src/main/java/com/wasup/mytasks/exception/CustomExceptionHandler.java
+++ src/main/java/com/wasup/mytasks/exception/CustomExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.wasup.mytasks.exception;
 
+import com.wasup.mytasks.model.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -8,22 +11,36 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class CustomExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleResourceNotFoundException(Exception e) {
-        return ResponseEntity.notFound().build();
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(Exception e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(Exception e) {
+        ErrorResponse errorResponse = new ErrorResponse().status(HttpStatus.NOT_FOUND.value()).message(e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<String> handleValidationException(Exception e) {
-        return ResponseEntity.internalServerError().body(e.getMessage());
+    public ResponseEntity<ErrorResponse> handleValidationException(Exception e) {
+        ErrorResponse errorResponse =
+                new ErrorResponse().status(HttpStatus.INTERNAL_SERVER_ERROR.value()).message(e.getMessage());
+        return ResponseEntity.internalServerError().body(errorResponse);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(Exception e) {
+        ErrorResponse errorResponse =
+                new ErrorResponse().status(HttpStatus.BAD_REQUEST.value()).message(e.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(Exception e) {
+        ErrorResponse errorResponse =
+                new ErrorResponse().status(HttpStatus.BAD_REQUEST.value()).message(e.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception e) {
-        return ResponseEntity.internalServerError().body(e.getMessage());
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        ErrorResponse errorResponse =
+                new ErrorResponse().status(HttpStatus.INTERNAL_SERVER_ERROR.value()).message(e.getMessage());
+        return ResponseEntity.internalServerError().body(errorResponse);
     }
 }
