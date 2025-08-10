153db766210fdcb0ed9146f734ddfe26ba51e343
Guillaume DHOTEL <guillaume.dhotel1@gmail.com>
2025-08-10 23:01:43 +0200

    refactor: consolidate exception handling and add support for additional exceptions

--- src/main/java/com/wasup/mytasks/exception/CustomExceptionHandler.java
+++ src/main/java/com/wasup/mytasks/exception/CustomExceptionHandler.java
@@ -4,8 +4,11 @@ import com.wasup.mytasks.model.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
@@ -16,28 +19,16 @@ public class CustomExceptionHandler {
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<ErrorResponse> handleValidationException(Exception e) {
-        ErrorResponse errorResponse =
-                new ErrorResponse().status(HttpStatus.INTERNAL_SERVER_ERROR.value()).message(e.getMessage());
-        return ResponseEntity.internalServerError().body(errorResponse);
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
+    @ExceptionHandler({IllegalArgumentException.class, MethodArgumentNotValidException.class,
+            HttpMessageNotReadableException.class, NoResourceFoundException.class,
+            HttpRequestMethodNotSupportedException.class})
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(Exception e) {
         ErrorResponse errorResponse =
                 new ErrorResponse().status(HttpStatus.BAD_REQUEST.value()).message(e.getMessage());
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(Exception e) {
-        ErrorResponse errorResponse =
-                new ErrorResponse().status(HttpStatus.BAD_REQUEST.value()).message(e.getMessage());
-        return ResponseEntity.badRequest().body(errorResponse);
-    }
-
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler({ValidationException.class, Exception.class})
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
         ErrorResponse errorResponse =
                 new ErrorResponse().status(HttpStatus.INTERNAL_SERVER_ERROR.value()).message(e.getMessage());
